c501a69b74461aa9c357a492ee66b2022f9ffe69
Nathan <Nathansilva18@outlook.com>
2022-08-24 20:57:02 -0300

    Adicionar arquivos de projeto.

---
+++ Fiap.Aula03.Web/Controllers/LoginController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Fiap.Aula03.Web.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Logar()
+        {
+            return View();
+        }
+    }
+}
---
+++ Fiap.Aula03.Web/Controllers/ProdutoController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Fiap.Aula03.Web.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Cadastrar()
+        {
+            return View();
+        }
+    }
+}
