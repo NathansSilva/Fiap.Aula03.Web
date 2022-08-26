eb2a2e4837f6b406d445ee5f8680b34a0dd0b726
Nathan <Nathansilva18@outlook.com>
2022-08-25 23:16:23 -0300

    Exercício calculadora

---
+++ Fiap.Aula03.Web/Controllers/CalculadoraController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Fiap.Aula03.Web.Controllers
+{
+    public class CalculadoraController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Calculadora()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Calculadora(int number, int number2)
+        {
+            
+            ViewBag.abobora = number + number2;
+            return View("Resultado");
+        }
+    }
+}
---
+++ Fiap.Aula03.Web/Models/Resultado.cs
@@ -0,0 +1,8 @@
+﻿namespace Fiap.Aula03.Web.Models
+{
+    public class Resultado
+    {
+        public int Number { get; set; }
+        public int Number2 { get; set; }
+    }
+}
